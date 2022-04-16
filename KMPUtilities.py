936a350357f432c278aab450d7032710fc203b34
Gabriela-Orzechowska <gabriela.orzechowska.2889@gmail.com>
2022-04-16 20:44:50 +0200

    Update KMPUtilities.py

--- KMPUtilities.py
+++ KMPUtilities.py
@@ -13,16 +13,53 @@ import bpy
 import math
 import struct 
 from bpy_extras.io_utils import ImportHelper
-                    
+
+
+lastselection = []
+
+    
+def updateArea(self, context):
+    scene = context.scene
+    mytool = scene.kmpt
+    activeObject = bpy.context.active_object
+    if(lastselection == activeObject):
+        if(activeObject.name.startswith("AREA_")): 
+            name = activeObject.name
+            properties = name.split("_")
+            if(mytool.kmp_areaEnumType != 'A0'):
+                mytool.kmp_areaID = -1
+            areaName = properties[0] + "_" + properties[1] + "_"  + properties[2] + "_"  + str(mytool.kmp_areaEnumType[1:]) + "_"  + str(mytool.kmp_areaID) + "_" +\
+            str(mytool.kmp_areaPrority) + "_"  + mytool.kmp_areaSet1 + "_"  + mytool.kmp_areaSet2 + "_"  + mytool.kmp_areaRoute + "_"  + mytool.kmp_areaEnemy
+            bpy.context.active_object.name = areaName
+                   
 class MyProperties(bpy.types.PropertyGroup):
     
     scale : bpy.props.FloatProperty(name= "Scale", soft_min= 0.0001, soft_max= 100000, default= 100)
+    #kmp_areaType : bpy.props.IntProperty(name = "Type", default= 0, update=updateArea)
+    kmp_areaEnumType : bpy.props.EnumProperty(name = "Type", items=[("A0", "Camera", ''),
+                                                                    ("A1", "EnvEffect", ''),
+                                                                    ("A2", "BFG Entry Swapper", ''),
+                                                                    ("A3", "Moving Road", ''),
+                                                                    ("A4", "Destination Point", ''),
+                                                                    ("A5", "Minimap Control", ''),
+                                                                    ("A6", "Music Changer", ''),
+                                                                    ("A7", "Flying Boos", ''),
+                                                                    ("A8", "Object Grouper", ''),
+                                                                    ("A9", "Group Unloading", ''),
+                                                                    ("A10", "Fall Boundary", '')],
+                                                                    update=updateArea)
+    kmp_areaID : bpy.props.IntProperty(name = "CAME", default= 0, update=updateArea)
+    kmp_areaPrority : bpy.props.IntProperty(name = "Priority", default= 0, update=updateArea)
+    kmp_areaSet1 : bpy.props.StringProperty(name = "Set1", default= "0", update=updateArea)
+    kmp_areaSet2 : bpy.props.StringProperty(name = "Set2", default= "0", update=updateArea)
+    kmp_areaRoute : bpy.props.StringProperty(name = "Route", default= "0", update=updateArea)
+    kmp_areaEnemy : bpy.props.StringProperty(name = "Enemy point ID", default= "0", update=updateArea)
  
-
+    
         
 class KMPUtilities(bpy.types.Panel):
     bl_label = "KMP Utilities"
-    bl_idname = "PT_KMP"
+    bl_idname = "_PT_KMP"
     bl_space_type = "VIEW_3D"
     bl_region_type = "UI"
     bl_category = "MKW Utils"
@@ -35,11 +72,33 @@ class KMPUtilities(bpy.types.Panel):
         layout.operator("kmpc.load")
         layout.operator("kmpc.cursor")
         layout.operator("kmpc.gobj")
-        layout.label(text="AREA")
+        
+class AREAUtilities(bpy.types.Panel):
+    bl_label = "AREA Utilities"
+    bl_idname = "_PT_KMP_AREA"
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "UI"
+    bl_category = "MKW Utils"
+    
+    def draw(self, context):
+        layout = self.layout
+        scene = context.scene
+        mytool = scene.kmpt
         layout.operator("kmpc.area")
-        area_create_column = layout.column();
+        layout.label(text="Create AREA")
+        area_create_column = layout.column()
         area_create_column.operator("kmpc.c_cube_area")
         area_create_column.operator("kmpc.c_cylinder_area")
+        
+        layout.label(text="AREA Settings")
+        area_setting_column = layout.column()
+        area_setting_column.prop(mytool, "kmp_areaEnumType")
+        area_setting_column.prop(mytool, "kmp_areaID")
+        area_setting_column.prop(mytool, "kmp_areaPrority")
+        area_setting_column.prop(mytool, "kmp_areaSet1")
+        area_setting_column.prop(mytool, "kmp_areaSet2")
+        area_setting_column.prop(mytool, "kmp_areaRoute")
+        area_setting_column.prop(mytool, "kmp_areaEnemy")
         
         if(bpy.context.object is not None):
             current_mode = bpy.context.object.mode
@@ -113,6 +172,23 @@ class kmp_area (bpy.types.Operator):
         x = 0
         for object in selected:
             object_position = object.location
+            name = object.name
+            properties = name.split("_")
+            areaNumber = "%02d" % (int(properties[1],))
+            areaShape = "%02d" % (int(properties[2],))
+            areaType = "%02d" % (int(properties[3],))
+            areaID = int(float(properties[4]))
+            if(str(areaID) == "-1"):
+                areaID = "FF"
+            else:
+                areaID = "%02d" % (int(float(properties[4])))
+            
+            areaPrority = "%02d" % (int(properties[5],))
+            areaSet1 = format(int(properties[6], 16), '04x')
+            areaSet2 = format(int(properties[7], 16), '04x')
+            areaSet = str(areaSet1) + str(areaSet2)
+            areaRoute = format(int(properties[8], 16), '04x')
+            areaEnemy = format(int(properties[9], 16), '04x')
             xpos = round(object_position[0] * scale, 2)
             ypos = round(object_position[2] * scale, 2)
             zpos = round(object_position[1] * scale * -1, 2)
@@ -122,10 +198,13 @@ class kmp_area (bpy.types.Operator):
             xscl = round(object.scale[0],2)
             yscl = round(object.scale[2],2)
             zscl = round(object.scale[1],2)
-            position = str(x) + "\t00\t00\tFF\t00\t" + str(xpos) + "\t" + str(ypos) + "\t" + str(zpos) + "\t" + str(xrot) + "\t" + str(yrot) + "\t" + str(zrot) + "\t" + str(xscl) + "\t" + str(yscl) + "\t" + str(zscl) + "\t00000000\tFFFF\t0000\n"
-            data = data + position
+            dataValue = str("%02d" % x) + "\t" + str(areaShape) + "\t" + str(areaType) + "\t" + str(areaID) + "\t" + str(areaPrority)  + "\t" +\
+            str(xpos) + "\t" + str(ypos) + "\t" + str(zpos) + "\t" + str(xrot) + "\t" + str(yrot) + "\t" + str(zrot) + "\t" + str(xscl) + "\t" + str(yscl) + "\t" + str(zscl) +\
+            "\t" + str(areaSet) + "\t" + str(areaRoute) + "\t" + str(areaEnemy) + "\n"
+            data = data + dataValue
             x = x + 1
-        
+            
+            print(areaSet)
         bpy.context.window_manager.clipboard = data
         return {'FINISHED'}
 
@@ -140,12 +219,27 @@ class kmp_c_cube_area (bpy.types.Operator):
         scale = mytool.scale
         cursor_position = context.scene.cursor.location
         
+        existingAreas = 0;
+        for obj in bpy.data.objects:
+            if "area_" in obj.name.lower():
+                existingAreas = existingAreas + 1
+        
         bpy.ops.mesh.primitive_cube_add(size=10000/scale, location=cursor_position)
         bpy.ops.object.mode_set(mode='EDIT')
         bpy.ops.mesh.select_all(action='SELECT')
         bpy.ops.transform.translate(value=(0,0,5000/scale), orient_type='GLOBAL')
         bpy.ops.object.mode_set(mode='OBJECT')
         activeObject = bpy.context.active_object
+        activeObject.name = "AREA_" + str(existingAreas) + "_0_0_0_0_0_0_FFFF_0"
+        name = activeObject.name
+        properties = name.split("_")
+        mytool.kmp_areaEnumType = "A" + properties[3]
+        mytool.kmp_areaID = int(float(properties[4]))
+        mytool.kmp_areaPrority = int(properties[5])
+        mytool.kmp_areaSet1 = properties[6]
+        mytool.kmp_areaSet2 = properties[7]
+        mytool.kmp_areaRoute = properties[8]
+        mytool.kmp_areaEnemy = properties[9]
         mat = bpy.data.materials.get("kmpc.area")
         if mat is None:
           mat = bpy.data.materials.new(name="kmpc.area")
@@ -167,12 +261,27 @@ class kmp_c_cylinder_area (bpy.types.Operator):
         scale = mytool.scale
         cursor_position = context.scene.cursor.location
         
+        existingAreas = 0;
+        for obj in bpy.data.objects:
+            if "area_" in obj.name.lower():
+                existingAreas = existingAreas + 1
+        
         bpy.ops.mesh.primitive_cylinder_add(radius=5000/scale, depth=10000/scale, location=cursor_position)
         bpy.ops.object.mode_set(mode='EDIT')
         bpy.ops.mesh.select_all(action='SELECT')
         bpy.ops.transform.translate(value=(0,0,5000/scale), orient_type='GLOBAL')
         bpy.ops.object.mode_set(mode='OBJECT')
         activeObject = bpy.context.active_object
+        activeObject.name = "AREA_" + str(existingAreas) + "_1_0_0_0_0_0_FFFF_0"
+        name = activeObject.name
+        properties = name.split("_")
+        mytool.kmp_areaEnumType = "A" + properties[3]
+        mytool.kmp_areaID = int(float(properties[4]))
+        mytool.kmp_areaPrority = int(properties[5])
+        mytool.kmp_areaSet1 = properties[6]
+        mytool.kmp_areaSet2 = properties[7]
+        mytool.kmp_areaRoute = properties[8]
+        mytool.kmp_areaEnemy = properties[9]
         mat = bpy.data.materials.get("kmpc.area")
         if mat is None:
           mat = bpy.data.materials.new(name="kmpc.area")
@@ -183,7 +292,7 @@ class kmp_c_cylinder_area (bpy.types.Operator):
         activeObject.data.materials.append(mat)
         return {'FINISHED'}
   
-      
+loading = 0
 class load_kmp(bpy.types.Operator, ImportHelper):
     bl_idname = "kmpc.load"
     bl_label = "Load KMP file"       
@@ -197,7 +306,8 @@ class load_kmp(bpy.types.Operator, ImportHelper):
         scene = context.scene
         mytool = scene.kmpt
         scale = mytool.scale
-
+        global loading
+        loading = 1
         
         path = self.filepath
         file = open(path, "rb")
@@ -214,6 +324,12 @@ class load_kmp(bpy.types.Operator, ImportHelper):
         for i in range(int(sectionNumber)):
             sectionOffsets.append(struct.unpack(">I", file.read(4))[0])
         print(sectionOffsets)
+        
+        existingAreas = 0;
+        for obj in bpy.data.objects:
+            if "area_" in obj.name.lower():
+                existingAreas = existingAreas + 1
+        
         areaOffset = 80 + sectionOffsets[9]
         file.seek(areaOffset, 0)
         areaNumber = struct.unpack('>H', file.read(2))[0]
@@ -222,9 +338,9 @@ class load_kmp(bpy.types.Operator, ImportHelper):
         areas = []
         for i in range(int(areaNumber)):
             areaShape = struct.unpack('>b', file.read(1))[0]
-            areaType = struct.unpack('>b', file.read(1))
-            areaCAME = struct.unpack('>b', file.read(1))
-            areaPriority = struct.unpack('>b', file.read(1))
+            areaType = struct.unpack('>b', file.read(1))[0]
+            areaCAME = struct.unpack('>b', file.read(1))[0]
+            areaPriority = struct.unpack('>b', file.read(1))[0]
             areaXPos = struct.unpack('>f', file.read(4))[0]
             areaYPos = struct.unpack('>f', file.read(4))[0]
             areaZPos = struct.unpack('>f', file.read(4))[0]
@@ -234,18 +350,23 @@ class load_kmp(bpy.types.Operator, ImportHelper):
             areaXScale = struct.unpack('>f', file.read(4))[0]
             areaYScale = struct.unpack('>f', file.read(4))[0]
             areaZScale = struct.unpack('>f', file.read(4))[0]
-            areaSet = struct.unpack('>I', file.read(4))[0]
+            areaSet1 = struct.unpack('>H', file.read(2))[0]
+            areaSet2 = struct.unpack('>H', file.read(2))[0]
             areaRoute = struct.unpack('>H', file.read(2))[0]
             areaEnemy = struct.unpack('>H', file.read(2))[0]
             
             areaLocation = (areaXPos/scale, areaZPos/scale * -1, areaYPos/scale)
             areaRotation = (math.radians(areaXRot), math.radians(areaZRot), math.radians(areaYRot))
             areaScale = (areaXScale, areaZScale, areaYScale)
-            areaName = "AREA" 
+            
+            areaName = "AREA_" + str(existingAreas + i) + "_" + '{:X}'.format(areaShape) + "_" + str(int(areaType)) + "_" + str(int(areaCAME))\
+             + "_" + '{:X}'.format(areaPriority) + "_" + '{:X}'.format(areaSet1) + "_" + '{:X}'.format(areaSet2) + "_" + '{:X}'.format(areaRoute) + "_" + '{:X}'.format(areaEnemy)
+             
+            areaName = areaName.upper()
             if(str(areaShape) == "0"):
                 bpy.ops.mesh.primitive_cube_add(size=10000/scale, location=areaLocation, rotation=areaRotation)
-                cube = bpy.context.selected_objects[0]
-                cube.name = "AREA_" + str(i)
+                obj = bpy.context.selected_objects[0]
+                obj.name = areaName
                 bpy.ops.object.mode_set(mode='EDIT')
                 bpy.ops.mesh.select_all(action='SELECT')
                 bpy.ops.transform.translate(value=(0,0,5000/scale), orient_type='GLOBAL')
@@ -253,7 +374,8 @@ class load_kmp(bpy.types.Operator, ImportHelper):
                 bpy.ops.transform.resize(value=areaScale, orient_type='LOCAL')      
             if(str(areaShape) == "1"):
                 bpy.ops.mesh.primitive_cylinder_add(radius=5000/scale, depth=10000/scale, location=areaLocation, rotation=areaRotation)
-                
+                obj = bpy.context.selected_objects[0]
+                obj.name = areaName
                 bpy.ops.object.mode_set(mode='EDIT')
                 bpy.ops.mesh.select_all(action='SELECT')
                 bpy.ops.transform.translate(value=(0,0,5000/scale), orient_type='GLOBAL')
@@ -268,26 +390,45 @@ class load_kmp(bpy.types.Operator, ImportHelper):
             activeObject.data.materials.append(mat)
             
         
-        
+        loading = 0
         return {'FINISHED'}
 
-    
-    
-classes = [MyProperties, KMPUtilities, cursor_kmp, kmp_gobj, kmp_area, kmp_c_cube_area, kmp_c_cylinder_area, load_kmp]
+
+
+def update_scene_handler(scene):
+    global lastselection
+    mytool = scene.kmpt
+    activeObject = bpy.context.active_object
+    if(loading == 0):
+        if(activeObject.name.startswith("AREA_")):
+            if(lastselection != activeObject):
+                name = activeObject.name
+                properties = name.split("_")
+                mytool.kmp_areaEnumType = "A" + properties[3]
+                mytool.kmp_areaID = int(float(properties[4]))
+                mytool.kmp_areaPrority = int(properties[5])
+                mytool.kmp_areaSet1 = properties[6]
+                mytool.kmp_areaSet2 = properties[7]
+                mytool.kmp_areaRoute = properties[8]
+                mytool.kmp_areaEnemy = properties[9]
+            
+    lastselection = activeObject
+
+classes = [MyProperties, KMPUtilities, AREAUtilities, cursor_kmp, kmp_gobj, kmp_area, kmp_c_cube_area, kmp_c_cylinder_area, load_kmp]
  
  
  
 def register():
     for cls in classes:
         bpy.utils.register_class(cls)
-        
-        bpy.types.Scene.kmpt = bpy.props.PointerProperty(type= MyProperties)
+    bpy.app.handlers.depsgraph_update_post.append(update_scene_handler)
+    bpy.types.Scene.kmpt = bpy.props.PointerProperty(type= MyProperties)
  
 def unregister():
     for cls in classes:
         bpy.utils.unregister_class(cls)
-        del bpy.types.Scene.my_tool
- 
+    bpy.app.handlers.depsgraph_update_post.remove(update_scene_handler)
+    del bpy.types.Scene.my_tool 
  
  
 if __name__ == "__main__":
