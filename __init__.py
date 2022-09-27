551de2a3b9f94cb4879b40b61b50e2fc14da9ed0
Gabriela-Orzechowska <gabriela.orzechowska.2889@gmail.com>
2022-09-27 18:52:21 +0200

    version

--- __init__.py
+++ __init__.py
@@ -662,9 +662,9 @@ labelDict = {
 
 }
 
-current_version = "v0.1.10-pre"
-latest_version = "v0.1.10-pre"
-prerelease_version = "v0.1.10-pre"
+current_version = "v0.1.10"
+latest_version = "v0.1.10"
+prerelease_version = "v0.1.10"
 
 kcl_typeATypes = ["T00","T01","T02","T03","T04","T05","T06","T07","T08","T09","T0A","T16","T17","T1D"]
 kcl_wallTypes = ["T0C","T0D","T0E","T0F","T1E","T1F", "T19"]
